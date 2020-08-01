8243ce45e2bdf8c2c9fa5c21ca4a5d2cf83b2f0c
Sam Ingle <sam.ingle87@googlemail.com>
2020-08-01 07:31:06 +0100

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Sam James Ingle on 01/08/2020.
+//  Copyright © 2020 Sam James Ingle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
